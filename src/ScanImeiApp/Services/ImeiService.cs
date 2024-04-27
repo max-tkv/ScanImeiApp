5c9d3a393d0f29dd30b5e372e270504b2be5b8f0
Терехов Максим Анатольевич <TerehovMA@akbarsdigital.ru>
2024-04-27 11:34:02 +0300

    Удален избыточный функционал определения похожих IMEI

--- src/ScanImeiApp/Services/ImeiService.cs
+++ src/ScanImeiApp/Services/ImeiService.cs
@@ -29,15 +29,6 @@ public class ImeiService : IImeiService
         List<RecognizeResult> recognizeResults, 
         CancellationToken cancellationToken)
     {
-        // if (_appOptions.EnabledSimilarImei)
-        // {
-        //     RemoveImeiSimilar(recognizeResult, resultAll);
-        // }
-        // else
-        // {
-        //     resultAll.AddRange(recognizeResult);
-        // }
-        
         RecognizeResult bestRecognizeResult = recognizeResults
             .OrderByDescending(x => x.Confidence)
             .First();
@@ -62,98 +53,6 @@ public class ImeiService : IImeiService
     }
 
     #region Приватные методы
-
-    /// <summary>
-    /// Удалить похожие IMEI.
-    /// </summary>
-    /// <remarks>
-    /// IMEI (International Mobile Equipment Identity) — это уникальный идентификатор мобильного устройства.
-    /// Каждый IMEI состоит из 15 цифр. В случае наличия двух IMEI на одном телефоне
-    /// (например, у некоторых моделей смартфонов с поддержкой двух SIM-карт),
-    /// они могут отличаться только в ПОСЛЕДНИХ 3 цифрах.
-    /// </remarks>
-    /// <param name="recognizeImeiList">IMEI для добаления.</param>
-    /// <param name="resultAll">Уже добавленные IMEI.</param>
-    /// <returns>True - похожи. False - не похожи.</returns>
-    private void RemoveImeiSimilar(IEnumerable<string> recognizeImeiList, List<string> resultAll)
-    {
-        foreach (var recognizeImei in recognizeImeiList)
-        {
-            if (!resultAll.Any())
-            {
-                resultAll.Add(recognizeImei);
-                continue;
-            }
-                    
-            foreach (var resultItem in resultAll.ToList())
-            {
-                if (AreImeiSimilar(recognizeImei, resultItem) &&
-                    resultAll.All(x => x != recognizeImei))
-                {
-                    resultAll.Add(recognizeImei);
-                }
-            }
-        }
-    }
-    
-    /// <summary>
-    /// Определить похоже ли IMEI. Отличаются последние 6 символов.
-    /// </summary>
-    /// <param name="imei1">IMEI 1.</param>
-    /// <param name="imei2">IMEI 2.</param>
-    /// <returns>True - похожи. False - не похожи.</returns>
-    private bool AreImeiSimilar(string imei1, string imei2)
-    {
-        if (imei1 == imei2)
-        {
-            return false;
-        }
-        
-        if (EqualFirstPartImei(imei1, imei2, 9) & !EqualLastPartImei(imei1, imei2, 6))
-        {
-            return true;
-        }
-
-        return false;
-    }
-
-    /// <summary>
-    /// Проверить первую часть IMEI (10 цыфр).
-    /// </summary>
-    /// <param name="imei1">IMEI 1.</param>
-    /// <param name="imei2">IMEI 2.</param>
-    /// <param name="numberCount">Количество последних сиволов.</param>
-    /// <returns>True - равны. False - не равны.</returns>
-    private static bool EqualLastPartImei(string imei1, string imei2, int numberCount)
-    {
-        int length = imei1.Length;
-        if (imei1[(length - numberCount)..] != imei2[(length - numberCount)..])
-        {
-            return false;
-        }
-
-        return true;
-    }
-
-    /// <summary>
-    /// Прверить последню часть IMEI (5 цыфр).
-    /// </summary>
-    /// <param name="imei1">IMEI 1.</param>
-    /// <param name="imei2">IMEI 2.</param>
-    /// <param name="numberCount">Количество первых символов.</param>
-    /// <returns>True - равны. False - не равны.</returns>
-    private static bool EqualFirstPartImei(string imei1, string imei2, int numberCount)
-    {
-        for (int i = 0; i < numberCount; i++)
-        {
-            if (imei1[i] != imei2[i])
-            {
-                return false;
-            }
-        }
-
-        return true;
-    }
     
     /// <summary>
     /// Записать логи с результатами изъятия текста с изображения.
